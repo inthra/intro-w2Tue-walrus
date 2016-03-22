150d0246849d9ab1442cc5b3a0ff67ba45e837f4
Inthrayuth Mahaphol and kate hall <inthrayuth@>
2016-03-22 10:40:43 -0700

    add peek-a-boo function

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,8 @@
 $().ready(function(){
-  $("h1").click(function(){
-    alert("This is the header.");
+
+  $(".clickable").click(function(){
+    $(".initially-hidden").toggle();
+    $(".initially-shown").toggle();
   });
-  $("h3").click(function(){
-    alert("This is the sub header.");
-  });
-  $("p").click(function(){
-    alert("This is the paragraph.");
-  });
-  $("#walrus1").click(function(){
-    alert("This is an example of click.");
-  });
-  $("ul").click(function() {
-    alert("this is a list");
-  });
-  $("#test").click(function() {
-    alert("we are masters");
-  });
-  $("#walrus2").dblclick(function(){
-    alert("This is an example of double click.");
-  });
-  $("ol").hover(function(){
-    alert("This is a hover test.");
-  });
+
 });
