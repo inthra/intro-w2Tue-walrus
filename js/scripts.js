587b0883a8ebaf66cc942326d061b92c67f62b0d
Inthrayuth Mahaphol and kate hall <inthrayuth@>
2016-03-22 11:35:24 -0700

    add add & remove class function

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,19 @@ $().ready(function(){
     $(".initially-hidden").fadeToggle();
     $(".initially-shown").fadeToggle();
   });
+
+  $("button#green").click(function(){
+    $("body").removeClass();
+    $("body").addClass("background-green");
+  });
+
+  $("button#yellow").click(function(){
+    $("body").removeClass();
+    $("body").addClass("background-yellow");
+  });
+
+  $("button#red").click(function(){
+    $("body").removeClass();
+    $("body").addClass("background-red");
+  });
 });
