5c323a76d2185038496c20a0888911252cbbcdcf
Inthrayuth Mahaphol and kate hall <kate@>
2016-03-22 09:37:09 -0700

    add double click, hover

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,19 @@ $().ready(function(){
   $("p").click(function(){
     alert("This is the paragraph.");
   });
-  $("img").click(function(){
-    alert("This is the image.");
+  $("#walrus1").click(function(){
+    alert("This is an example of click.");
+  });
+  $("ul").click(function() {
+    alert("this is a list");
+  });
+  $("#test").click(function() {
+    alert("we are masters");
+  });
+  $("#walrus2").dblclick(function(){
+    alert("This is an example of double click.");
+  });
+  $("ol").hover(function(){
+    alert("This is a hover test.");
   });
 });
