b220808bea2ad9bf4ddb36fad1ec39101b0bea5a
Inthrayuth Mahaphol and kate hall <kate@>
2016-03-22 09:14:55 -0700

    add scripts.js and add html

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$().ready(function(){
+  $("h1").click(function(){
+    alert("This is the header.");
+  });
+  $("h3").click(function(){
+    alert("This is the sub header.");
+  });
+  $("p").click(function(){
+    alert("This is the paragraph.");
+  });
+  $("img").click(function(){
+    alert("This is the image.");
+  });
+});
